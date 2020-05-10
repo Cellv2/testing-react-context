cd4bdfdc7a371e96e258d3aa103512718ffd750d
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-10 20:02:04 +0100

    Add input form to update global state

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../stores/store";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
 const InputComponent = (props: Props) => {
-    return <div>This is the input component</div>;
+    // @ts-ignore
+    const [state, setState] = useContext(Context);
+
+    const [_name, _setName] = useState({});
+    const [_email, _setEmail] = useState({});
+
+    const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const data = {
+            name: _name,
+            email: _email,
+        };
+
+        setState(data);
+    };
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const key = event.target.name;
+        const value = event.target.value;
+
+        switch (key) {
+            case "name":
+                _setName(value);
+                return;
+            case "email":
+                _setEmail(value);
+                return;
+            default:
+                throw new Error();
+        }
+    };
+
+    return (
+        <>
+            <h2>Submit form to update global state</h2>
+            <form onSubmit={handleOnSubmit}>
+                <div>
+                    <label htmlFor="name">Name: </label>
+                    <input
+                        name="name"
+                        type="text"
+                        onChange={handleOnChange}
+                        placeholder={state.name}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        name="email"
+                        type="text"
+                        onChange={handleOnChange}
+                        placeholder={state.email}
+                    />
+                </div>
+                <button>Update data</button>
+            </form>
+            <br />
+            <Link to="/">Check on the global state</Link>
+        </>
+    );
 };
 
 export default InputComponent;
