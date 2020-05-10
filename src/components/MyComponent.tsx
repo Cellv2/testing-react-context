77d2abd3aabdf984759b304b7d74e7ec38cdea18
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-10 19:19:32 +0100

    Add component to use global state

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "../stores/store";
 
 const MyComponent = () => {
-    return <div>MyComponent</div>;
+    //@ts-ignore
+    const [state, setState] = useContext(Context);
+
+    const handleOnClick = async (): Promise<void> => {
+        const request = await fetch("https://randomuser.me/api/");
+        const response = await request.json();
+        const personData = response.results[0];
+
+        const person = {
+            name: `${personData.name.first} ${personData.name.last}`,
+            email: personData.email,
+        };
+
+        setState(person);
+
+        return;
+    };
+
+    return (
+        <>
+            <h2>MyComponent</h2>
+            <p>Name: {state.name}</p>
+            <p>Email: {state.email}</p>
+            <button onClick={handleOnClick}>Click</button>
+        </>
+    );
 };
 
 export default MyComponent;
